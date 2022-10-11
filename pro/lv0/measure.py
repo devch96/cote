d84da3b77fa239476caca9405c28ed64b218e4f7
devch96 <chjung1006@gmail.com>
2022-10-11 06:59:48 +0000

    약수 구하기

---
+++ pro/lv0/measure.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = []
+    for i in range(1,int((n**(1/2)+1))):
+        if n % i == 0:
+            answer.append(i)
+            if (i**2) != n:
+                answer.append(int(n/i))
+    answer.sort()
+    return answer
+
