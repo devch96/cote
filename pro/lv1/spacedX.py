81553a60336d3f0555b55e3bef497b3feb2b11d3
devch96 <chjung1006@gmail.com>
2022-10-19 14:07:10 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ pro/lv1/spacedX.py
@@ -0,0 +1,7 @@
+def solution(x,n):
+    if x > 0 :
+        return [i for i in range(x,x*n+1,x)]
+    elif x == 0:
+        return [0 for i in range(n)]
+    else:
+        return [i for i in range(x,x*n-1,x)]
