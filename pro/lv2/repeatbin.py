490f28c3e25b6cfc92f0ba73fca75139de6d893b
devch96 <chjung1006@gmail.com>
2022-10-26 22:20:19 +0900

    이진 변환 반복하기

--- pro/lv2/repeatbin.py
+++ pro/lv2/repeatbin.py
@@ -3,7 +3,7 @@ def solution(s):
     change = 0
     while s != "1":
         zero_count += s.count('0')
-        s = s.replace('0','')
+        s.replace('0','')
         s = str(bin(len(s))[2:])
         change+=1
     return [change, zero_count]
