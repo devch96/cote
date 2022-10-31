514b88c9fd56f7cc96b78d3e19f3ffa2d74114b5
devch96 <chjung1006@gmail.com>
2022-10-31 14:02:01 +0900

    위장

---
+++ pro/lv2/camoflage.py
@@ -0,0 +1,7 @@
+from collections import counter
+def solution(clothes):
+    kinds = Counter(list(zip(*clothes))[1])
+    answer = 1
+    for i in kinds.values():
+        answer *= (i+1)
+    return answer-1 
