c2b6c9ebfee259e7577820fa22e0267d48a8a335
devch96 <chjung1006@gmail.com>
2022-11-04 15:25:43 +0900

    [1차] 뉴스 클러스터링

--- pro/lv2/clustring.py
+++ pro/lv2/clustring.py
@@ -17,41 +17,5 @@ def solution(str1, str2):
     print (inter, union)
     answer = int((len(inter)/len(union))*65536)
     return answer
-
-
-# def solution(str1, str2):
-#     s1 = make_2_word(str1)
-#     s2 = make_2_word(str2)
-#     if s1 == [] and s2 == []:
-#         return 65536
-
-#     s1_copy = s1.copy()
-#     s2_copy = s2.copy()
-
-#     # 교집합
-#     inter = []
-#     for i in s1:
-#         if i in s2_copy:
-#             inter.append(i)
-#             s1_copy.remove(i)
-#             s2_copy.remove(i)
-
-
-#     # 합집합
-#     union = inter + s1_copy + s2_copy
-
-
-#     answer = int((len(inter) / len(union)) * 65536)
-#     print(inter,union)
-#     return answer
-
-
-# def make_2_word(s):
-#     s = s.upper()
-#     a = []
-#     for i in range(len(s) - 1):
-#         if s[i:i + 2].isalpha():
-#             a.append(s[i:i + 2])
-#     return a
 print(solution("FRANCE","french"))
 print(solution("handshake","shake hands"))
