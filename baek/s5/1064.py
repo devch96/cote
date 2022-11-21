a28b12b2a579af4870fb0dfd8d7052cb6af3e15c
devch96 <chjung1006@gmail.com>
2022-11-21 14:33:04 +0900

    평행사변형

---
+++ baek/s5/1064.py
@@ -0,0 +1,12 @@
+Xa, Ya, Xb, Yb, Xc, Yc = map(int,input().split())
+answer = 0.0
+if ((Xa-Xb)*(Ya-Yc)==(Ya-Yb)*(Xa-Xc)):
+    print(-1.0)
+    exit(0)
+def hypot(x, y):
+    return ((x**2) + (y**2))**0.5
+a = hypot(Xa-Xb, Ya-Yb)
+b = hypot(Xb-Xc, Yb-Yc)
+c = hypot(Xc-Xa, Yc-Ya)
+answer = ((max(a+b, b+c, c+a) - min(a+b,b+c,c+a))*2)
+print(answer)
