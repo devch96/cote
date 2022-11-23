e471de19221af8677e34dcc05a68c97e6c6f244a
devch96 <chjung1006@gmail.com>
2022-11-23 16:56:07 +0900

    1233 주사위

---
+++ baek/b2/1233.py
@@ -0,0 +1,7 @@
+s1, s2, s3 = map(int,input().split())
+tmp = [0]*81
+for i in range(1,s1+1):
+    for j in range(1,s2+1):
+        for k in range(1,s3+1):
+            tmp[i+j+k] += 1
+print(tmp.index(max(tmp)))
