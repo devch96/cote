c53d8f373801d5029be6aabec99e958a1cf09131
devch96 <chjung1006@gmail.com>
2022-12-09 18:04:32 +0900

    14425 문자열 집합

---
+++ baek/s3/14425.py
@@ -0,0 +1,12 @@
+#문자열 집합
+import sys
+input = sys.stdin.readline
+n , m = map(int,input().split())
+s = set()
+for _ in range(n):
+    s.add(input().rstrip())
+cnt = 0
+for _ in range(m):
+    if input().rstrip() in s:
+        cnt += 1
+print(cnt)
