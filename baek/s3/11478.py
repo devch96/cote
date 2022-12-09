459c1c9bfc49b063cb4301086748e8be307c44a7
devch96 <chjung1006@gmail.com>
2022-12-09 18:13:36 +0900

    11478 서로 다른 부분 문자열의 개수

---
+++ baek/s3/11478.py
@@ -0,0 +1,9 @@
+#서로 다른 부분 문자열의 개수
+import sys
+input = sys.stdin.readline
+s = input().rstrip()
+a = set()
+for i in range(len(s)):
+    for j in range(i,len(s)):
+        a.add(s[i:j+1])
+print(len(a))
