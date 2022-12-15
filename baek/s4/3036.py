5f8e3bc432afb7ad344d3cee4574fab54f3f22ba
devch96 <chjung1006@gmail.com>
2022-12-15 16:26:59 +0900

    3036 링

---
+++ baek/s4/3036.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+ring = list(map(int,input().split()))
+def gcd(x,y):
+    while y:
+        x,y = y, x%y
+    return x
+std = ring[0]*2
+for i in range(1,n):
+    denominator = ring[i]*2
+    numerator = std
+    measure = gcd(denominator, numerator)
+    answer = '{nume}/{deno}'.format(nume = numerator//measure, deno = denominator//measure)
+    print(answer)
