b28afbac3ba287978f06f5f095c612988e4979e5
devch96 <chjung1006@gmail.com>
2022-12-15 16:08:14 +0900

    1934 최소공배수

---
+++ baek/b1/1934.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+def gcd(x,y):
+    while y:
+        x,y = y,x%y
+    return x
+def lcm(x,y):
+    return (x*y)//gcd(x,y)
+t = int(input())
+for _ in range(t):
+    a,b = map(int,input().split())
+    print(lcm(a,b))
