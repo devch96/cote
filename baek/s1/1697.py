91e0edecda88e2d21ba491d4492fef602c86b966
devch96 <chjung1006@gmail.com>
2023-05-09 18:27:21 +0900

    숨바꼭질

--- baek/s1/1697.py
+++ baek/s1/1697.py
@@ -1,6 +1,20 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-N,K = map(int,input().split())
-q = deque()
-q.append(N)
+n,k = map(int,input().split())
+M = 100000
+check = [0] * 100001
+def bfs():
+    q = deque()
+    q.append(n)
+    while q:
+        x = q.popleft()
+        if x == k:
+            print(check[x])
+            break
+        for nx in [x-1, x+1, x*2]:
+            if 0 <= nx <= M and not check[nx]:
+                q.append(nx)
+                check[nx] = check[x]+1
+
+bfs()
